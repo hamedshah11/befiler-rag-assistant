3c4c688c621feea2b3d83a826e5c5761cda65814
hamedshah11 <137182035+hamedshah11@users.noreply.github.com>
2025-05-26 00:19:39 -0700

    Create BefilerRagAssistantApplication.java

---
+++ src/main/java/com/befiler/assistant/BefilerRagAssistantApplication.java
@@ -0,0 +1,11 @@
+package com.befiler.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BefilerRagAssistantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BefilerRagAssistantApplication.class, args);
+    }
+}
